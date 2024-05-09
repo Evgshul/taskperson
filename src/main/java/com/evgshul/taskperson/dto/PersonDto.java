af52c85c9cb6fe5b803f36dd6ac2b15fdcb6bf64
evgenys <evgshul@gmail.com>
2024-05-09 11:13:47 +0300

    Updated spring boot up to 3.2.5 and added springdoc-openapi, updated dto to shema example for swagger ui

--- src/main/java/com/evgshul/taskperson/dto/PersonDto.java
+++ src/main/java/com/evgshul/taskperson/dto/PersonDto.java
@@ -2,6 +2,7 @@ package com.evgshul.taskperson.dto;
 
 import com.evgshul.taskperson.model.Gender;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -13,6 +14,8 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -21,20 +24,24 @@ public class PersonDto {
 
     @NotNull
     @Pattern(regexp = "(^[\\p{L}\\s'.-]+$)", message = "Not valid first name and last name")
+    @Schema(name = "fullname", example = "Name Surname", requiredMode = REQUIRED)
     private String fullName;
 
     @NotNull
     @JsonFormat(pattern="dd/MM/yyyy")
     @Past(message = "date of birth must be less than today")
+    @Schema(name = "birthdate", type = "string", pattern = "dd/MM/yyyy", example = "12/02/1998", requiredMode = REQUIRED)
     private LocalDate birthdate;
 
     private Gender gender;
 
     @NotNull
     @Pattern(regexp = "^[0-9]*$", message = "Phone number should use only digits")
+    @Schema(name = "phonenumber", example = "00371256412", requiredMode = REQUIRED)
     private String phoneNumber;
 
     @NotNull
     @Email(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$", message = "wrong email value")
+    @Schema(name = "email", example = "nickname@mail.com", requiredMode = REQUIRED)
     private String email;
 }
